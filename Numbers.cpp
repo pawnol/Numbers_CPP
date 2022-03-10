a6d354f69011101ee4db861459e35f0fff479ca0
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-10 10:24:19 -0600

    Add files via upload

---
+++ Numbers.cpp
@@ -0,0 +1,17 @@
+/*
+Numbers
+Pawelski
+3/14/2021
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+
+int main()
+{
+    int digit1 = 1;
+    int digit2 = 6;
+    std::cout << digit1 << digit2 << "\n";
+    return 0;
+}
